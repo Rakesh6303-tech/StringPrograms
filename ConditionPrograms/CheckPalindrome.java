078ebdab458384ca3b0d04313d870ce3c0d12a71
Rakesh <dr8452799@gmail.com>
2025-07-04 21:15:38 +0530

    Create CheckPalindrome.java

---
+++ ConditionPrograms/CheckPalindrome.java
@@ -0,0 +1,29 @@
+/*
+WRite a program to check Palindrome or Not?
+// */
+ import java.util.*;
+public class demo{
+  public static void main(String args []){
+    Scanner scan = new Scanner(System.in);
+    String s = scan.nextLine();
+    System.out.print(toceckPalindrome(s));
+  }
+  static String toceckPalindrome(String s){
+    // Traveresal String from Right To Let
+    int i=0, j=s.length()-1;
+    while(i<j){
+      // If Both Characters are equal
+      if(s.charAt(i)==s.charAt(j))
+      {
+        i++;
+        j--;
+    }
+        // Not Equal 
+    else{
+      return "Not palindrome";
+    }
+  }
+  // Correct Palindrome
+  return "Palindrome";
+}
+ }
