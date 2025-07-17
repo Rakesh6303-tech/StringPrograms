6429162d29bc585f44412cd8d725f6cc6a6facab
Rakesh <dr8452799@gmail.com>
2025-07-17 14:48:22 +0530

    Create AllSubStrings.java

---
+++ SubStrings/AllSubStrings.java
@@ -0,0 +1,34 @@
+Input : ron
+  output : r
+           o
+           n
+           r o
+           o n
+           r o n
+  
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      //System.out.println("Hello, World!");
+      Scanner scan = new Scanner(System.in);
+      String s = scan.nextLine();
+      PrintAllSubStrings(s);
+    }
+    static void PrintAllSubStrings(String s)
+    {
+      for(int size=1;size<=s.length();size++){
+        
+      for(int i=0;i<s.length()-size;i++)
+        {
+          String t = "";
+          for(int j=i;j<i+size;j++)
+          {
+            t = t+ s.charAt(j);
+          }
+        System.out.println(t);
+      }
+      } 
+    }
+
+    
+  }
